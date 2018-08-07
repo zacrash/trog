539395f650a93175c352c21f38a5cc575d910f8e
Brad Squicciarini <bradsquicciarini@github.com>
2018-08-07 16:55:50 -0700

    Removes now ignored directory 'build' and 'devel'

--- ros/build/bruinwalker_mapping/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/caev/Desktop/bruinwalker/ros/src/bruinwalker_mapping
-# Build directory: /home/caev/Desktop/bruinwalker/ros/build/bruinwalker_mapping
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
