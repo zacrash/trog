0bbafd2b16e7f01f9ea68524c4da0ea8ee43194c
Brad Squicciarini <bradsquicciarini@gmail.com>
2018-08-29 12:28:31 -0700

    Start diff_drive_controller development

---
+++ ros/src/troggie_control/troggie_control/README.md
@@ -0,0 +1,9 @@
+Troggie_Control TODOs
+=====
+
+## trog_hardware.cc
+    Implement writeCommandsToHardware()
+    Implement updateJointsFromHardware()
+
+## Implement trog_control_node.cc
+    Give it control loop sheeeeeet
---
+++ ros/src/troggie_control/troggie_control/include/trog_control/trog_hardware.h
@@ -0,0 +1,73 @@
+
+#ifndef TROG_HARDWARE_H
+#define TROG_HARDWARE_H
+
+#include "hardware_interface/joint_state_interface.h"
+#include "hardware_interface/joint_command_interface.h"
+#include "hardware_interface/robot_hw.h"
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+#include "roboteq_driver/controller.h"
+
+namespace trog_base
+{
+
+  /**
+  * Class representing Trog hardware, allows for ros_control to modify internal state via joint interfaces
+  */
+  class TrogHardware : public hardware_interface::RobotHW
+  {
+    public:
+      TrogHardware(ros::NodeHandle nh, ros::NodeHandle private_nh, double target_control_freq);
+
+      void updateJointsFromHardware();
+
+      void writeCommandsToHardware();
+
+      void reportLoopDuration(const ros::Duration &duration);
+
+    private:
+    
+      void resetTravelOffset();
+
+      void registerControlInterfaces();
+
+      double linearToAngular(const double &travel) const;
+
+      double angularToLinear(const double &angle) const;
+
+      void limitDifferentialSpeed(double &travel_speed_left, double &travel_speed_right);
+
+      ros::NodeHandle nh_, private_nh_;
+
+      // Roboteq motor controller
+      roboteq::Controller controller_;
+
+      // ROS Control interfaces
+      hardware_interface::JointStateInterface joint_state_interface_;
+      hardware_interface::VelocityJointInterface velocity_joint_interface_;
+
+      // ROS Parameters
+      double wheel_diameter_, max_accel_, max_speed_;
+
+      double polling_timeout_;
+
+      /**
+      * Joint structure that is hooked to ros_control's InterfaceManager, to allow control via diff_drive_controller
+      */
+      struct Joint
+      {
+        double position;
+        double position_offset;
+        double velocity;
+        double effort;
+        double velocity_command;
+
+        Joint() :
+          position(0), velocity(0), effort(0), velocity_command(0)
+        { }
+      } joints_[2];
+    };
+
+}  // namespace trog_base
+#endif  // TROG_HARDWARE_H
