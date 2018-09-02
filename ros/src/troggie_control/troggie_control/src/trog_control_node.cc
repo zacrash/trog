92cdc981b4afdb198fccd02de25eed9c3aa16264
Brad Squicciarini <bradsquicciarini@gmail.com>
2018-09-01 19:40:27 -0700

    Not so minor

--- ros/src/troggie_control/troggie_control/src/trog_control_node.cc
+++ ros/src/troggie_control/troggie_control/src/trog_control_node.cc
@@ -29,7 +29,7 @@ void controlLoop(trog_control::TrogHardware &troggie,
 
 int main(int argc, char *argv[])
 {
-  ros::init(argc, argv, "husky_base");
+  ros::init(argc, argv, "trog_base");
   ros::NodeHandle nh, private_nh("~");
 
   double control_frequency, diagnostic_frequency;
@@ -37,25 +37,25 @@ int main(int argc, char *argv[])
   private_nh.param<double>("diagnostic_frequency", diagnostic_frequency, 1.0);
 
   // Initialize robot hardware and link to controller manager
-  husky_base::HuskyHardware husky(nh, private_nh, control_frequency);
-  controller_manager::ControllerManager cm(&husky, nh);
+  trog_control::TrogHardware troggie(nh, private_nh, control_frequency);
+  controller_manager::ControllerManager cm(&troggie, nh);
 
   // Setup separate queue and single-threaded spinner to process timer callbacks
   // that interface with Husky hardware - libhorizon_legacy not threadsafe. This
   // avoids having to lock around hardware access, but precludes realtime safety
   // in the control loop.
-  ros::CallbackQueue husky_queue;
-  ros::AsyncSpinner husky_spinner(1, &husky_queue);
+  ros::CallbackQueue trog_queue;
+  ros::AsyncSpinner trog_spinner(1, &trog_queue);
 
   time_source::time_point last_time = time_source::now();
   ros::TimerOptions control_timer(
     ros::Duration(1 / control_frequency),
-    boost::bind(controlLoop, boost::ref(husky), boost::ref(cm), boost::ref(last_time)),
-    &husky_queue);
+    boost::bind(controlLoop, boost::ref(troggie), boost::ref(cm), boost::ref(last_time)),
+    &trog_queue);
   ros::Timer control_loop = nh.createTimer(control_timer);
 
 
-  husky_spinner.start();
+  trog_spinner.start();
 
   // Process remainder of ROS callbacks separately, mainly ControlManager related
   ros::spin();
